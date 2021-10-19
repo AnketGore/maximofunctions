11568b0b55e63f2d5c54d68ed508d61690e9a8cc
AnketGore <92137640+AnketGore@users.noreply.github.com>
2021-10-19 10:22:06 +0530

    Update local_test_of_function.py

--- scripts/local_test_of_function.py
+++ scripts/local_test_of_function.py
@@ -11,10 +11,8 @@ EngineLogging.configure_console_logging(logging.DEBUG)
 '''
 You can test functions locally before registering them on the server to
 understand how they work.
-
 Supply credentials by pasting them from the usage section into the UI.
 Place your credentials in a separate file that you don't check into the repo. 
-
 '''
 
 with open('../credentials_as.json', encoding='utf-8') as F:
@@ -24,27 +22,22 @@ db = Database(credentials=credentials)
 
 '''
 Import and instantiate the functions to be tested 
-
 The local test will generate data instead of using server data.
 By default it will assume that the input data items are numeric.
-
 Required data items will be inferred from the function inputs.
-
 The function below executes an expression involving a column called x1
 The local test function will generate data dataframe containing the column x1
-
 By default test results are written to a file named df_test_entity_for_<function_name>
 This file will be written to the working directory.
-
 '''
 
-from multiplebyfactorAGG.functions import MultipleByFactorAGG
+from multiplebyfactorAGG.functions import multiplebyfactorAGG
 
-fn = MultipleByFactorAGG(name='AS_Tester', greeting_col='greeting')
+fn = multiplebyfactorAGG(name='AS_Tester', greeting_col='greeting')
 fn.execute_local_test(db=db, db_schema=db_schema)
 
 '''
 Register function so that you can see it in the UI
 '''
 
-db.register_functions([MultipleByFactorAGG])
+db.register_functions([multiplebyfactorAGG])
